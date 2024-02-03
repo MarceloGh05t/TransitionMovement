baa437c7ce44d518ea303c10624204af3c207835
Marcelo Gh05t <38195894+MarceloGh05t@users.noreply.github.com>
2024-02-03 02:11:41 -0300

    Add files via upload

---
+++ TransitionMovement/edittime.js
@@ -0,0 +1,166 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Transition Movement",			// as appears in 'add behavior' dialog, can be changed as long as "id" stays the same
+		"id":			"TransitionMovement",			// this is used to identify this behavior and is saved to the project; never change it
+		"version":		"1.0",					// (float in x.y format) Behavior version - C2 shows compatibility warnings based on this
+		"description":	"Creates transition movement.",
+		"author":		"M. J. Borghi",
+		"help url":		"",
+		"category":		"Movements",				// Prefer to re-use existing categories, but you can set anything here
+		"flags":		0						// uncomment lines to enable flags...
+						| bf_onlyone			// can only be added once to an object, e.g. solid
+	};
+};
+
+////////////////////////////////////////
+// Parameter types:
+// AddNumberParam(label, description [, initial_string = "0"])			// a number
+// AddStringParam(label, description [, initial_string = "\"\""])		// a string
+// AddAnyTypeParam(label, description [, initial_string = "0"])			// accepts either a number or string
+// AddCmpParam(label, description)										// combo with equal, not equal, less, etc.
+// AddComboParamOption(text)											// (repeat before "AddComboParam" to add combo items)
+// AddComboParam(label, description [, initial_selection = 0])			// a dropdown list parameter
+// AddObjectParam(label, description)									// a button to click and pick an object type
+// AddLayerParam(label, description)									// accepts either a layer number or name (string)
+// AddLayoutParam(label, description)									// a dropdown list with all project layouts
+// AddKeybParam(label, description)										// a button to click and press a key (returns a VK)
+// AddAudioFileParam(label, description)								// a dropdown list with all imported project audio files
+
+////////////////////////////////////////
+// Conditions
+
+// AddCondition(id,					// any positive integer to uniquely identify this condition
+//				flags,				// (see docs) cf_none, cf_trigger, cf_fake_trigger, cf_static, cf_not_invertible,
+//									// cf_deprecated, cf_incompatible_with_triggers, cf_looping
+//				list_name,			// appears in event wizard list
+//				category,			// category in event wizard list
+//				display_str,		// as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>, and {my} for the current behavior icon & name
+//				description,		// appears in event wizard dialog when selected
+//				script_name);		// corresponding runtime function name
+				
+// example				
+AddCondition(0, cf_none, "Is moving", "Check Movement", "Is moving", "Is moving transition", "IsMoving")
+
+AddCondition(1, cf_none, "In destiny", "Check Movement", "arrived at destination", "arrived at destination", "in_destiny")
+
+AddCondition(2, cf_none, "Enabled", "Check Enabled", "is Enabled", "Check if it is Enabled", "enabled")
+
+AddCondition(3, cf_none, "Is horizontal moving", "Check Movement", "Is horizontal moving", "Is horizontal moving", "is_horizontal_moving")
+
+AddCondition(4, cf_none, "Is vertical moving", "Check Movement", "Is vertical moving", "Is vertical moving", "is_vertical_moving")
+
+AddCondition(5, cf_none, "Horizontal type", "Movement type", "Horizontal type", "Horizontal move type", "horizontal_type")
+
+AddCondition(6, cf_none, "Vertical type", "Movement type", "Vertical type", "Vertical move type", "vertical_type")
+
+////////////////////////////////////////
+// Actions
+
+// AddAction(id,				// any positive integer to uniquely identify this action
+//			 flags,				// (see docs) af_none, af_deprecated
+//			 list_name,			// appears in event wizard list
+//			 category,			// category in event wizard list
+//			 display_str,		// as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>
+//			 description,		// appears in event wizard dialog when selected
+//			 script_name);		// corresponding runtime function name
+
+// example
+AddAction(0, af_none, "Start", "Movement", "Enabled movement", "Enabled movement", "Start")
+
+AddAction(1, af_none, "Stop", "Movement", "Disabled movement", "Disabled movement", "stop")
+
+AddNumberParam("Enf point X", "Value in pixel to end point X.", "0")
+AddAction(2, af_none, "Change end point x", "Value", "Change end point x to {0}", "Change end point x value to horizontal movement", "change_end_pointx")
+
+AddNumberParam("Enf point Y", "Value in pixel to end point Y.", "0")
+AddAction(3, af_none, "Change end point y", "Value", "Change end point y to {0}", "Change end point y value to vertical movement", "change_end_pointy")
+
+AddAction(4, af_none, "Horizontal movement", "Type Movement", "Change to horizontal movement", "Change to horizontal movement", "change_horizontal_movement")
+
+AddAction(5, af_none, "Vertical movement", "Type Movement", "Change to vertical movement", "Change to vertical movement", "change_vertical_movement")
+
+AddNumberParam("Acceleration", "The rate of acceleration, in pixels per second per second", "400")
+AddAction(6, af_none, "Change Acceleration", "Value", "Change Acceleration to {0}", "Change Acceleration", "change_acceleration")
+
+////////////////////////////////////////
+// Expressions
+
+// AddExpression(id,			// any positive integer to uniquely identify this expression
+//				 flags,			// (see docs) ef_none, ef_deprecated, ef_return_number, ef_return_string,
+//								// ef_return_any, ef_variadic_parameters (one return flag must be specified)
+//				 list_name,		// currently ignored, but set as if appeared in event wizard
+//				 category,		// category in expressions panel
+//				 exp_name,		// the expression name after the dot, e.g. "foo" for "myobject.foo" - also the runtime function name
+//				 description);	// description in expressions panel
+
+// example
+AddExpression(0, ef_return_number, "End point x", "Values", "end_pointX", "Return the number of end poin x.")
+AddExpression(1, ef_return_number, "End point y", "Values", "end_pointY", "Return the number of end poin y.")
+AddExpression(2, ef_return_number, "Acceleration", "Values", "acceleration", "Return the number of acceleration.")
+AddExpression(3, ef_return_string, "Type movement", "Movement", "type_movement", "Return the string of type movement.")
+
+////////////////////////////////////////
+ACESDone();
+
+////////////////////////////////////////
+// Array of property grid properties for this plugin
+// new cr.Property(ept_integer,		name,	initial_value,	description)		// an integer value
+// new cr.Property(ept_float,		name,	initial_value,	description)		// a float value
+// new cr.Property(ept_text,		name,	initial_value,	description)		// a string
+// new cr.Property(ept_combo,		name,	"Item 1",		description, "Item 1|Item 2|Item 3")	// a dropdown list (initial_value is string of initially selected item)
+
+var property_list = [
+	new cr.Property(ept_combo, "Initial state", "Enabled", "Whether to initially have the behavior enabled or disabled.", "Disabled|Enabled"),
+	new cr.Property(ept_float, "End Point X", 0, "The movement Horizontal will be towards the end point x."),
+	new cr.Property(ept_float, "End Point Y", 0, "The movement Vertical will be towards the end point y."),
+	new cr.Property(ept_combo, "Movement", "Horizontal", "Type of movement.", "Horizontal|Vertical"),
+	new cr.Property(ept_float, "Acceleration", 400, "The rate of acceleration, in pixels per second per second.")
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of the behavior in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// any other properties here, e.g...
+	// this.myValue = 0;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ TransitionMovement/runtime.js
@@ -0,0 +1,372 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+// *** CHANGE THE BEHAVIOR ID HERE *** - must match the "id" property in edittime.js
+//           vvvvvvvvvv
+cr.behaviors.TransitionMovement = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	// *** CHANGE THE BEHAVIOR ID HERE *** - must match the "id" property in edittime.js
+	//                               vvvvvvvvvv
+	var behaviorProto = cr.behaviors.TransitionMovement.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+		
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+		// Load properties
+		this.enabled = (this.properties[0] !== 0) // o valor de retorno é 0 ou 1
+		this.active = "" //para passar o valor do enabled como true ou false na funcao tick
+		this.acceleration = this.properties[4]
+		this.moving = false
+		this.destiny = false
+		this.type_movement = ""
+		
+		// object is sealed after this call, so make sure any properties you'll ever need are created, e.g.
+		// this.myValue = 0;
+	};
+	
+	behinstProto.onDestroy = function ()
+	{
+		// called when associated object is being destroyed
+		// note runtime may keep the object and behavior alive after this call for recycling;
+		// release, recycle or reset any references here as necessary
+	};
+	
+	// called when saving the full state of the game
+	behinstProto.saveToJSON = function ()
+	{
+		// return a Javascript object containing information about your behavior's state
+		// note you MUST use double-quote syntax (e.g. "property": value) to prevent
+		// Closure Compiler renaming and breaking the save format
+		return {
+			// e.g.
+			//"myValue": this.myValue
+		};
+	};
+	
+	// called when loading the full state of the game
+	behinstProto.loadFromJSON = function (o)
+	{
+		// load from the state previously saved by saveToJSON
+		// 'o' provides the same object that you saved, e.g.
+		// this.myValue = o["myValue"];
+		// note you MUST use double-quote syntax (e.g. o["property"]) to prevent
+		// Closure Compiler renaming and breaking the save format
+	};
+
+	behinstProto.tick = function ()
+	{
+		var dt = this.runtime.getDt(this.inst);
+		
+		// called every tick for you to update this.inst as necessary
+		// dt is the amount of time passed since the last tick, in case it's a movement
+		if(this.properties[0] !== 0){//o desable vai retornar zero e o enable 1
+		this.active = true
+		
+			if(this.properties[3] === 0){ //se for um movimento horizontal
+				this.type_movement = "Horizontal"
+				
+				if (this.properties[1] < this.inst.x && this.acceleration != 0){ // se o ponto final for menor que o x (o ponto final estiver a esquerda do do objeto)
+					this.inst.x -= this.acceleration * dt // coloquei pelo dt para atulizar pelo intervalo de tempo entre um tick como o dt é uma fracao tive que colocar mais casas na aceleração
+					this.inst.set_bbox_changed()
+					this.moving = true
+			
+					
+						if (this.properties[1] >= this.inst.x){// se o movimento, quando passar o ponto final ou for chegar no ponto 
+							
+							this.inst.x = this.properties[1] //vai setar exatamente para o ponto final, se tiver passado dele
+							this.inst.set_bbox_changed() // atualizar a renderização
+							this.moving = false
+							this.destiny = true
+							
+							
+						}
+				}
+				if (this.properties[1] > this.inst.x && this.acceleration != 0){ // se o ponto final for maior que o x (o ponto final estiver a direita do do objeto)
+				
+				this.inst.x += this.acceleration * dt 
+				this.inst.set_bbox_changed()
+				this.moving = true
+			
+				
+					if (this.properties[1] <= this.inst.x){// se o movimento, quando passar o ponto final ou for chegar no ponto 
+							
+							this.inst.x = this.properties[1] //vai setar exatamente para o ponto final, se tiver passado dele
+							this.inst.set_bbox_changed() // atualizar a renderização
+							this.moving = false
+							this.destiny = true
+						}
+				}
+			}
+			if(this.properties[3] === 1){ //se for um movimento vertical
+			
+				this.type_movement = "Vertical"
+				
+				if (this.properties[2] < this.inst.y && this.acceleration != 0){ // se o ponto final for menor que o y (o ponto final estiver a acima do do objeto)
+				
+				this.inst.y -= this.acceleration * dt  
+				this.inst.set_bbox_changed()
+				this.moving = true
+				
+					if (this.properties[2] >= this.inst.y){// se o movimento, quando passar o ponto final ou for chegar no ponto 
+						
+						this.inst.y  = this.properties[2] //vai setar exatamente para o ponto final, se tiver passado dele
+						this.inst.set_bbox_changed() // atualizar a renderização
+						this.moving = false
+						this.destiny = true
+					}
+				}
+				if (this.properties[2] > this.inst.y && this.acceleration != 0){// se o ponto final for maior que o y (o ponto final estiver a abaixo do do objeto)
+				
+				this.inst.y += this.acceleration * dt  
+				this.inst.set_bbox_changed()
+				this.moving = true
+					
+					if (this.properties[2] <= this.inst.y){// se o movimento, quando passar o ponto final ou for chegar no ponto 
+					
+						this.inst.y  = this.properties[2] //vai setar exatamente para o ponto final, se tiver passado dele
+						this.inst.set_bbox_changed() // atualizar a renderização
+						this.moving = false
+						this.destiny = true
+					}
+				}
+	
+			
+			}
+		}else{
+		this.active = false
+		}
+	};
+	
+	// The comments around these functions ensure they are removed when exporting, since the
+	// debugger code is no longer relevant after publishing.
+	/**BEGIN-PREVIEWONLY**/
+	behinstProto.getDebuggerValues = function (propsections)
+	{
+		// Append to propsections any debugger sections you want to appear.
+		// Each section is an object with two members: "title" and "properties".
+		// "properties" is an array of individual debugger properties to display
+		// with their name and value, and some other optional settings.
+		propsections.push({
+			"title": this.type.name,
+			"properties": [
+				// Each property entry can use the following values:
+				// "name" (required): name of the property (must be unique within this section)
+				// "value" (required): a boolean, number or string for the value
+				// "html" (optional, default false): set to true to interpret the name and value
+				//									 as HTML strings rather than simple plain text
+				// "readonly" (optional, default false): set to true to disable editing the property
+				{"name": "Enable", "value": this.active},
+				{"name": "Moving", "value": this.moving},
+				{"name": "Type Moving", "value": this.type_movement},
+				{"name": "In destiny", "value": this.destiny},
+				{"name": "End Point X", "value": this.properties[1]},
+				{"name": "End Point Y", "value": this.properties[2]},
+				{"name": "Acceleration", "value": this.properties[4]}
+				
+			]
+		});
+	};
+	
+	behinstProto.onDebugValueEdited = function (header, name, value)
+	{
+		// Called when a non-readonly property has been edited in the debugger. Usually you only
+		// will need 'name' (the property name) and 'value', but you can also use 'header' (the
+		// header title for the section) to distinguish properties with the same name.
+		
+		//if (name === "My property")
+			//this.myProperty = value;
+	};
+	/**END-PREVIEWONLY**/
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+
+	// the example condition
+	Cnds.prototype.IsMoving = function ()
+	{
+		
+		if(this.moving){
+		return true
+		}
+	};
+	
+	Cnds.prototype.in_destiny = function ()
+	{
+		
+		if(this.destiny){
+		return true
+		}
+	}
+	
+	Cnds.prototype.enabled = function ()
+	{
+		
+		if(this.active){
+		return true
+		}
+	}
+	
+	Cnds.prototype.is_horizontal_moving = function ()
+	{
+		
+		if(this.type_movement === "Horizontal" && this.moving){
+		return true
+		}
+	}
+	
+	Cnds.prototype.is_vertical_moving = function ()
+	{
+		
+		if(this.type_movement === "Vertical" && this.moving){
+		return true
+		}
+	}
+	
+	Cnds.prototype.horizontal_type = function ()
+	{
+		
+		if(this.properties[3] === 0){
+		return true
+		}
+	}
+	
+	Cnds.prototype.vertical_type = function ()
+	{
+		
+		if(this.properties[3] === 1){
+		return true
+		}
+	}
+	
+	// ... other conditions here ...
+	
+	behaviorProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+
+	// the example action
+	Acts.prototype.Start = function ()
+	{
+	
+			this.properties[0] = 1 //propriedade de enable
+	}
+	
+	Acts.prototype.stop = function ()
+	{
+	
+			this.properties[0] = 0 //propriedade de disable
+			this.moving = false
+	}
+	
+	Acts.prototype.change_end_pointx = function (posicao)
+	{
+	
+			this.properties[1] = posicao
+	}
+	
+	Acts.prototype.change_end_pointy = function (posicao)
+	{
+	
+			this.properties[2] = posicao
+	}
+	
+	Acts.prototype.change_horizontal_movement = function ()
+	{
+	
+			this.properties[3] = 0 // vai ficar como movimento horizontal
+	}
+	
+	Acts.prototype.change_vertical_movement = function ()
+	{
+	
+			this.properties[3] = 1 // vai ficar como movimento vertical
+	}
+	
+	Acts.prototype.change_acceleration = function (acceleration)
+	{
+	
+			this.acceleration = acceleration
+			this.properties[4] = acceleration
+	}
+	
+	// ... other actions here ...
+	
+	behaviorProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+
+	// the example expression
+	Exps.prototype.end_pointX = function (ret)	// 'ret' must always be the first parameter - always return the expression's result through it!
+	{
+		ret.set_float(this.properties[1])				// return value end point x
+	
+	}
+	
+	Exps.prototype.end_pointY = function (ret)
+	{
+		ret.set_float(this.properties[2])				// return value end point y
+	
+	}
+	
+	Exps.prototype.acceleration = function (ret)
+	{
+		ret.set_float(this.properties[4])				// return acceleration value
+	
+	}
+	
+	Exps.prototype.type_movement = function (ret)
+	{
+		ret.set_string(this.type_movement)				// return type movement
+	
+	}
+	
+		// ret.set_float(0.5);			// for returning floats
+		// ret.set_string("Hello");		// for ef_return_string
+		// ret.set_any("woo");			// for ef_return_any, accepts either a number or string
+	// ... other expressions here ...
+	
+	behaviorProto.exps = new Exps();
+	
+}());
